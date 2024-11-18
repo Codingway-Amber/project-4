8250a9c3be02b17e41662f6172476ffef791c5a6
Codingway-Amber <amberreine7@gmail.com>
2024-11-18 14:01:20 -0600

    added pink and font

--- js/main.js
+++ js/main.js
@@ -1,6 +1,32 @@
-const toggleButton = document.querySelector('.toggle-button');
-const navbar = document.querySelector('.navbar ul');
+const button = document.getElementById('submit');
 
-toggleButton.addEventListener('click', () => {
-  navbar.classList.toggle('active');
-});
+button.addEventListener("click", (ev) => {
+    // Prevent the default action of the event
+    ev.preventDefault();
+    // declare variables for use
+    const body = document.querySelector('body');
+    const nameOutput = document.getElementById('form-name');
+    console.log(nameOutput);
+    
+    // Get the value of the input field
+    const nameInput = document.getElementById('name');
+    const nameValue = nameInput.value;
+    console.log('nameInput:', nameInput);
+    console.log('nameValue:', nameValue);
+    nameOutput.innerHTML = nameValue;
+    // alert('You clicked me!, ' + nameValue);
+    
+    // Perform action: add class to body element
+    body.classList.add('form-submitted');
+    
+    window.scrollTo(0, 0);
+}); 
+
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "flex") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "flex";
+    }
+  }
