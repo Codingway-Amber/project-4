e9971a59654a75a1cb293100915c1faac09a0a20
Codingway-Amber <amberreine7@gmail.com>
2024-11-18 22:36:45 -0600

    added pics and lightbox

--- js/main.js
+++ js/main.js
@@ -1,27 +1,3 @@
-const button = document.getElementById('submit');
-
-button.addEventListener("click", (ev) => {
-    // Prevent the default action of the event
-    ev.preventDefault();
-    // declare variables for use
-    const body = document.querySelector('body');
-    const nameOutput = document.getElementById('form-name');
-    console.log(nameOutput);
-    
-    // Get the value of the input field
-    const nameInput = document.getElementById('name');
-    const nameValue = nameInput.value;
-    console.log('nameInput:', nameInput);
-    console.log('nameValue:', nameValue);
-    nameOutput.innerHTML = nameValue;
-    // alert('You clicked me!, ' + nameValue);
-    
-    // Perform action: add class to body element
-    body.classList.add('form-submitted');
-    
-    window.scrollTo(0, 0);
-}); 
-
 function myFunction() {
     var x = document.getElementById("myLinks");
     if (x.style.display === "flex") {
@@ -30,3 +6,8 @@ function myFunction() {
       x.style.display = "flex";
     }
   }
+
+  $(document).on("click", '[data-toggle="lightbox"]', function(event) {
+    event.preventDefault();
+    $(this).ekkoLightbox();
+  });
