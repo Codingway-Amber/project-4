c7b5af70448373aa54a89202dc906c42641274f6
Codingway-Amber <amberreine7@gmail.com>
2024-12-02 11:20:13 -0600

    fixed hamburger menu

--- js/art.js
+++ js/art.js
@@ -1,3 +1,12 @@
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "flex") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "flex";
+  }
+};
+
 $(document).on("click", '[data-toggle="lightbox"]', function(event) {
     event.preventDefault();
     $(this).ekkoLightbox();
